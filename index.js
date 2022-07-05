4c19e10b0393eb6e6c52b48db4db0c789c025fc5
YasirAliSmit <103916303+YasirAliSmit@users.noreply.github.com>
2022-07-06 03:45:44 +0500

    Js 36to40 GIT

---
+++ index.js
@@ -0,0 +1,70 @@
+// //------------Question no 1------------------
+// var a = new Date();
+// document.write(a)
+
+//-------------Question no 2------------
+// var month = ["Janvary","febary","March","April","May","June","July","Augest","setembar","octubare","Novembr","Decembr"]
+// var a=new Date()
+// var b=month[a.getMonth()]
+// console.log(b)
+
+//--------------Question no 3---------------------
+// var day = ["Sun","Monday","tuesday","Wednesday","Thuesday","friday","Saturday"]
+// var d=new Date();
+// var c=day[d.getDay()]
+// console.log(c)
+
+
+// //--------------Question no 4------------
+// var day = ["Sun","Monday","tuesday","Wednesday","Thuesday","friday","Saturday"]
+// var a=new Date();
+// var b=day[a.getDay()]
+// if(b==day[0,6]){
+//     document.write("Today is Fun Day")
+//     }else{
+//         document.write(b)
+//     }
+
+
+
+// //---------------Question no 5--------------
+// var a = new Date();
+// var b= a.getDay();
+// if (b<=15){
+//     document.write("First 15 Day of the Month")
+// }else if (b>=15){
+//     document.write("last 15 Day of the Month")
+// }
+
+
+// //---------------------Question no 7-------------
+// var a = new Date()
+// var b = a.getHours()
+// if (b <=12){
+//     document.write("Its Am")
+// }else if ( b >= 12)
+// {
+//     document.write("Its Pm")
+// }
+
+//---------------------Question no 8--------------------
+// var a =new Date()
+// a.setDate(31)
+// document.write(a)
+
+//--------------Question no 10---------------
+// var a =new Date();
+// a.setSeconds(2015)
+// document.write(a)
+
+// //-----------Question no 12-----------------
+// var a=new Date()
+// a.getDate()
+// document.write("Current Date ",a,"<br>")
+// var b=new Date()
+// b.setDate("December 15 1990")
+// document.write("100 year back it was, ",b)
+
+
+
+
